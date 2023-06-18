9d3c00230de663a4b32746c2a1a69d2c60bdd3c3
DevilMayDead <965752122@qq.com>
2023-06-18 19:49:26 +0800

    first commit

---
+++ .dumirc.ts
@@ -0,0 +1,34 @@
+import chalk from 'chalk';
+import {defineConfig} from 'dumi';
+import {readdirSync} from 'fs';
+import {join} from 'path';
+
+const headPkgList: string[] = [];
+// utils must build before core
+// runtime must build before renderer-react
+const pkgList = readdirSync(join(__dirname, 'packages')).filter(
+    (pkg) => pkg.charAt(0) !== '.' && !headPkgList.includes(pkg),
+);
+
+const alias = pkgList.reduce((pre, pkg) => {
+    pre[`@selenium-design/${pkg}`] = join(__dirname, 'packages', pkg, 'src');
+    return {
+        ...pre,
+    };
+}, {});
+
+console.log(`🌼 alias list \n${chalk.blue(Object.keys(alias).join('\n'))}`);
+
+const tailPkgList = pkgList.map((path) => `packages/${path}/src/`);
+
+console.log(tailPkgList)
+
+console.log(alias,tailPkgList.map((dir) => ({type: 'component', dir})))
+
+export default defineConfig({
+    alias,
+    resolve: {
+        docDirs: ['docs'],
+        atomDirs: tailPkgList.map((dir) => ({type: 'component', dir})),
+    },
+})
